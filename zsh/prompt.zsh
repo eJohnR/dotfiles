0b6fe8832ff9521272a23b9544d1b7adab451716
John Rowley <John.A.Rowley@Googlemail.com>
2013-02-08 15:00:28 +0000

    added git branch to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,13 @@
 autoload colors && colors
 
-export PROMPT="%{$fg[red]%}%n%{$reset_color%} at %{$fg[cyan]%}%m%{$reset_color%} in %{$fg[green]%}%1~%{$reset_color%} "
+# Display Current Git Branch
+autoload -Uz vcs_info
+setopt promptsubst
+zstyle ':vcs_info:*' enable git svn
+zstyle ':vcs_info:git*' formats "%{$fg[black]%}[%{$reset_color%}%{$fg[yellow]%}%b%{$reset_color%}%{$fg[black]%}]%{$reset_color%}"
+
+# Format Prompt
+export PROMPT='%{$fg[red]%}%n%{$reset_color%} at %{$fg[cyan]%}%m%{$reset_color%} in %{$fg[green]%}%1~%{$reset_color%} on ${vcs_info_msg_0_}
+> '
+
+precmd() {vcs_info}
