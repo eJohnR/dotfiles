0ad458e46922e5698154a53d855980409e754dfa
John Rowley <John.A.Rowley@Googlemail.com>
2013-02-12 15:32:23 +0000

    Show staged changes in RPROMPT

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,10 +4,14 @@ autoload colors && colors
 autoload -Uz vcs_info
 setopt promptsubst
 zstyle ':vcs_info:*' enable git svn
-zstyle ':vcs_info:git*' formats "on %{$fg[black]%}[%{$reset_color%}%{$fg[yellow]%}%b%{$reset_color%}%{$fg[black]%}]%{$reset_color%}"
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' stagedstr '*'
+zstyle ':vcs_info:git*' formats "on %{$fg[black]%}[%{$reset_color%}%{$fg[yellow]%}%b%{$reset_color%}%{$fg[black]%}]%{$reset_color%}" "%{$fg[red]%}%c%{$reset_color%}"
 
 # Format Prompt
 export PROMPT="%{$fg[green]%}%2~%{$reset_color%} \${vcs_info_msg_0_}
 > "
+
+export RPROMPT="\${vcs_info_msg_1_}"
 
 precmd() {vcs_info}
