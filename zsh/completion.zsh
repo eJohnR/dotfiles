e83a3e2ecda792b41af93e3434eed36e8d522e2d
John Rowley <John.A.Rowley@Googlemail.com>
2013-02-13 00:21:55 +0000

    fixed git status colouring

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,2 +1,6 @@
 # Ignore case when autocompleting
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# Fuzzy matching of completions
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
