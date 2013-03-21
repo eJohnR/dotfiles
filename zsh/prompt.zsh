29e6484b3b7c72986bdf3518aa793c93e8f10cbe
John Rowley <John.A.Rowley@Googlemail.com>
2013-03-21 21:28:23 +0000

    Modified zsh prompt colouring

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,7 +7,7 @@ zstyle ':vcs_info:*' enable git svn
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr '*'
 zstyle ':vcs_info:*' unstagedstr '*'
-zstyle ':vcs_info:git*' formats "on %{$fg[black]%}[%{$reset_color%}%{$fg[yellow]%}%b%{$reset_color%}%{$fg[black]%}]%{$reset_color%}" "%{$fg[cyan]%}%c%{$reset_color%}%{$fg[red]%}%u%{$reset_color%}"
+zstyle ':vcs_info:git*' formats "on %{$fg[red]%}[%{$reset_color%}%{$fg[yellow]%}%b%{$reset_color%}%{$fg[red]%}]%{$reset_color%}" "%{$fg[cyan]%}%c%{$reset_color%}%{$fg[red]%}%u%{$reset_color%}"
 
 # Format Prompt
 export PROMPT="%{$fg[green]%}%2~%{$reset_color%} \${vcs_info_msg_0_}
