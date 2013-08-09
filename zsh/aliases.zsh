ac8bcfac7a9dd918427cd9bc3ad7097575a88d8e
John Rowley <John.A.Rowley@Googlemail.com>
2013-08-09 18:12:01 +0100

    Aliases for todo.sh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,8 @@ alias gd="git diff"
 alias gc="git commit"
 alias gco="git checkout"
 alias gsu="git status -uno"
+
+# todo.sh Aliases
+alias t="todo.sh"
+alias ta="todo.sh add"
+alias tc="todo.sh do"
