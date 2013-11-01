bd8ea08daa996c703f2e527fa0c29db314381017
John Rowley <John.A.Rowley@Googlemail.com>
2013-11-01 12:34:01 +0000

    Included aliases for Punchy.py, pmd.sh; Modified config files for zsh, vim, and tmux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,14 @@ alias gco="git checkout"
 alias gsu="git status -uno"
 
 # todo.sh Aliases
-alias td="todo.sh | sort"
+alias td="todo.sh"
 alias tda="todo.sh add"
 alias tdc="todo.sh do"
+
+# pmd.sh Aliases
+pmd() {
+   nohup pmd.sh $* > /dev/null &
+}
+
+# punch.py Aliases
+alias punch="Punch.py"
